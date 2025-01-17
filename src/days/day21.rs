7efb23257d5422d83f617417a26678ae5f565040
TruffleWolf <life4pylo@gmail.com>
2025-01-17 15:18:45 +0000

    Day22

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,9 +1,166 @@
-
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+use crate::vec2;
+// use std::collections::HashMap;
+// use std::collections::VecDeque;
 
 pub fn part1(){
     println!("Day21 Part1");
+    let char_grid = parse_document();
+    let mut relvance_grid:Vec<Vec<bool>>= Vec::with_capacity(char_grid.len());
+    let mut start_loc:[i16;2]=[0,0];
+    for y in 0..char_grid.len(){
+        let mut row:Vec<bool>= Vec::with_capacity(char_grid[y].len());
+        for x in 0..char_grid[y].len(){
+            row.push(false);
+            if char_grid[y][x] == 'S'{start_loc = [x as i16,y as i16];}
+        }
+        relvance_grid.push(row);
+    }
+    let mut parse_list:Vec<[i16;2]>=vec![start_loc];
+    for _i in 0..64{
+        let mut new_list:Vec<[i16;2]>=Vec::new();
+        for p in &parse_list{
+            relvance_grid[p[1]as usize][p[0]as usize]= true;
+            new_list = append_next_steps(&char_grid, p, new_list);
+        }
+        parse_list = new_list;
+    }
+
+    println!("Total:{}",parse_list.len());
+    
 }
 
 pub fn part2(){
     println!("Day21 Part2");
+    let char_grid = parse_document();
+    let target:i64 = 26501365;
+    let length:i64 = char_grid.len() as i64;
+    //let half:i64 = length/2;
+    
+    // let mut start_loc:[i16;2]=[0,0];
+    // 'y: for y in 0..char_grid.len(){
+    //     for x in 0..char_grid[y].len(){
+    //         if char_grid[y][x] == 'S'{
+    //             start_loc = [x as i16,y as i16];
+    //             break 'y;
+    //         }
+    //     }
+    // }
+
+    // let map1 = search(&char_grid, &start_loc, 64);
+    // let mut addition1:i64 = 0;
+    // for (distance,quant) in map1{
+    //     if distance%2==64%2{addition1+=quant;}
+    //     //addition1+=quant;
+    // }
+    // println!("Part1:{}",addition1);
+    // println!("{}",target%length);
+    let mut steps:Vec<i64>=Vec::new();
+    // for i in 0..3{
+    //     let count = half+i*length;
+    //     println!("remain{}",count%length);
+    //     let mut addition:i64 = 0;
+    //     let map = search(&char_grid, &start_loc, count);
+    //     for (distance,quant)in map{
+    //         if distance%2!=count%2{addition+=quant;}
+    //     }
+    //     steps.push(addition);
+    //     println!("{}/{}",i,addition);
+    // }
+
+    // let mut list:Vec<[i16;2]>=vec![start_loc];
+    // for i in 1..100000{
+    //     let mut new_list:Vec<[i16;2]> = Vec::new();
+    //     for loc in &list{
+    //         for dir in vec2::cardinals(){
+    //             let new_step:[i16;2]= [loc[0]+dir[0],loc[1]+dir[1]];
+    //             if char_grid[new_step[1]as usize %char_grid.len()][new_step[0]as usize%char_grid[0].len()]!='#' && !new_list.contains(&new_step){
+    //                 new_list.push(new_step);
+    //             }
+    //         }
+    //     }
+    //     list = new_list;
+    //     if i%length == target%length{println!("{}/{}",i,list.len());}
+    // }
+    steps.push(3957);
+    steps.push(35218);
+    steps.push(97615);
+    println!("{:?}",steps);
+    //let total = parse_quad( [steps[0],steps[1],steps[2]],(target-half)/length);
+    let total = parse_quad( [steps[0],steps[1],steps[2]],(target/length)+1);
+    //let total:i64 = (quads[0]*26501365*26501365)+(quads[1]*26501365)+quads[2];
+    //[65,196,327]
+    //[3694,34471,96363]
+    //[3957, 35218, 97615]mk2
+    //let total:u64 = 3694+34471*202300+202300*(202300-1)*96363/2;
+    println!("Total:{}",total);
+
+}
+
+// fn search(grid:&Vec<Vec<char>>,start:&[i16;2],depth:i64)->HashMap<i64,i64>{
+//     let mut new_map:HashMap<i64,i64>=HashMap::new();
+//     let mut seen:Vec<[i16;2]>=Vec::new();
+//     let mut to_parse:VecDeque<[i16;2]>=VecDeque::new();
+//     let mut to_count:VecDeque<i64>=VecDeque::new();
+//     to_parse.push_back(start.clone());
+//     to_count.push_back(0);
+//     while to_parse.len()>0{
+//         let loc = to_parse.pop_front().unwrap();
+//         let count = to_count.pop_front().unwrap();
+//         if count == depth+1 || seen.contains(&loc){continue;}
+//         let slot = new_map.entry(count).or_insert(0);
+//         *slot +=1;
+//         seen.push(loc);
+//         for dir in vec2::cardinals(){
+//             let new_step:[i16;2]= [loc[0]+dir[0],loc[1]+dir[1]];
+//             if grid[new_step[1]as usize %grid.len()][new_step[0]as usize%grid[0].len()]!='#'{
+//                 to_parse.push_back(new_step);
+//                 to_count.push_back(count+1);
+//             }
+//         }
+//     }
+//     return new_map;
+// }
+
+fn parse_quad(steps:[i64;3],part:i64)->i64{
+    // let a = (steps[2]-(2*steps[1])+steps[0])/2;
+    // let b = steps[1]-steps[0]-a;
+    // let c = steps[0];
+    // println!("{}/{}/{}",a,b,c);
+    // return (a*part*part)+(b*part)+c;
+    let a = steps[0];
+    let b = steps[1]-steps[0];
+    let c = steps[2]-steps[1];
+    return a + b*part + (part*(part-1)/2)*(c-b);
+    
+}
+
+fn append_next_steps(grid:&Vec<Vec<char>>,location:&[i16;2],known_list:Vec<[i16;2]>)->Vec<[i16;2]>{
+    let mut new_list = known_list;
+    for dir in vec2::cardinals(){
+        let new_step:[i16;2]= [location[0]+dir[0],location[1]+dir[1]];
+        if vec2::in_bounds(&new_step, &grid){
+            if !new_list.contains(&new_step) && grid[new_step[1]as usize][new_step[0]as usize] != '#'{
+                new_list.push(new_step);
+            }
+        }
+    }
+
+    return new_list;
+}
+
+fn parse_document()->Vec<Vec<char>>{
+    let mut output:Vec<Vec<char>>=Vec::new();
+    //let input_doc = File::open("./src/inputs//test/Day21test.txt").expect("File not found.");
+    let input_doc = File::open("./src/inputs/Day21input.txt").expect("File not found.");
+    let reader = BufReader::new(input_doc);
+
+    for line in reader.lines(){
+        let line_str = line.expect("Could not parse line");
+        let new_line:Vec<char> = line_str.chars().collect();
+        output.push(new_line);
+    }
+
+    return output;
 }
--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,9 +1,232 @@
-
+use std::cmp;
+use std::fs::File;
+use std::io::{BufRead,BufReader};
 
 pub fn part1(){
     println!("Day22 Part1");
+    let mut brick_list = parse_document();
+
+    let mut falling:bool = true;
+    println!("Parse complete");
+    //drop all the bricks
+    while falling{
+        falling = false;
+        for b in 0..brick_list.len(){
+            let mut can_fall = true;
+            'sq: for square in 0..brick_list[b].len(){
+                if brick_list[b][square][2]==1{
+                    can_fall= false;
+                    break;
+                }
+                let new_square =[brick_list[b][square][0],brick_list[b][square][1],brick_list[b][square][2]-1];
+                for alt_b in 0..brick_list.len(){
+                    if alt_b!=b{
+                        if brick_list[alt_b].contains(&new_square){
+                            can_fall = false;
+                            break 'sq;
+                        }
+                    }
+                }
+            }
+            if can_fall{
+                for s in 0..brick_list[b].len(){
+                    brick_list[b][s][2]= brick_list[b][s][2]-1;
+                }
+                falling = true;
+            }
+        }
+    }
+    println!("fall complete");
+    //calculate supports for each brick
+    let mut supp_list:Vec<Vec<usize>>=Vec::new();
+    let mut crit_list:Vec<u32>=Vec::new();
+    for b in 0..brick_list.len(){
+        let mut supports:Vec<usize> = Vec::new();
+
+        for square in 0..brick_list[b].len(){
+            if brick_list[b][square][2]==1{
+                supports = Vec::new();
+                break;
+            }
+            let new_square =[brick_list[b][square][0],brick_list[b][square][1],brick_list[b][square][2]-1];
+            for alt_b in 0..brick_list.len(){
+                if alt_b!=b{
+                    if brick_list[alt_b].contains(&new_square){
+                        if !supports.contains(&alt_b){
+                            supports.push(alt_b);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        supp_list.push(supports);
+        crit_list.push(0);
+    }
+    println!("Supports complete");
+    //count the relevant bricks
+
+    for s in &supp_list{
+        if s.len()==1{
+            crit_list[s[0]]+=1;
+        }
+    }
+    let mut total:u32 = 0;
+    for c in crit_list{
+        if c == 0{total+=1;}
+    }
+    println!("Total:{}",total);
+
 }
 
 pub fn part2(){
     println!("Day22 Part2");
+    let mut brick_list = parse_document();
+
+    let mut falling:bool = true;
+    println!("Parse complete");
+    //drop all the bricks
+    while falling{
+        falling = false;
+        for b in 0..brick_list.len(){
+            let mut can_fall = true;
+            'sq: for square in 0..brick_list[b].len(){
+                if brick_list[b][square][2]==1{
+                    can_fall= false;
+                    break;
+                }
+                let new_square =[brick_list[b][square][0],brick_list[b][square][1],brick_list[b][square][2]-1];
+                for alt_b in 0..brick_list.len(){
+                    if alt_b!=b{
+                        if brick_list[alt_b].contains(&new_square){
+                            can_fall = false;
+                            break 'sq;
+                        }
+                    }
+                }
+            }
+            if can_fall{
+                for s in 0..brick_list[b].len(){
+                    brick_list[b][s][2]= brick_list[b][s][2]-1;
+                }
+                falling = true;
+            }
+        }
+    }
+    println!("fall complete");
+    //calculate supports for each brick
+    let mut supp_list:Vec<Vec<usize>>=Vec::new();
+    let mut crit_list:Vec<u32>=Vec::new();
+    for b in 0..brick_list.len(){
+        let mut supports:Vec<usize> = Vec::new();
+
+        for square in 0..brick_list[b].len(){
+            if brick_list[b][square][2]==1{
+                supports = Vec::new();
+                break;
+            }
+            let new_square =[brick_list[b][square][0],brick_list[b][square][1],brick_list[b][square][2]-1];
+            for alt_b in 0..brick_list.len(){
+                if alt_b!=b{
+                    if brick_list[alt_b].contains(&new_square){
+                        if !supports.contains(&alt_b){
+                            supports.push(alt_b);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        supp_list.push(supports);
+        crit_list.push(0);
+    }
+    println!("Supports complete");
+    //count the relevant bricks
+
+    for s in &supp_list{
+        if s.len()==1{
+            crit_list[s[0]]+=1;
+        }
+    }
+    let mut total = 0;
+    for pos in 0..crit_list.len(){
+        if crit_list[pos] != 0{
+            let mut broken_list:Vec<usize> = vec![pos];
+            let mut breaking:bool = true;
+            while breaking{
+                breaking = false;
+                for s in 0..supp_list.len(){
+                    if s != pos && supp_list[s].len()>0 && !broken_list.contains(&s){
+                        let mut will_break = true;
+                        for support in &supp_list[s]{
+                            will_break = will_break && broken_list.contains(support);
+                        }
+                        if will_break{
+                            breaking = true;
+                            broken_list.push(s);
+                        }
+                    }
+                }
+            }
+
+            total+=broken_list.len()-1;
+        }
+        println!("{pos}");
+    }
+
+    println!("Total:{}",total);
+
+}
+
+
+fn parse_document()->Vec<Vec<[i16;3]>>{
+    let mut output:Vec<Vec<[i16;3]>>=Vec::new();
+    //let input_doc = File::open("./src/inputs//test/Day22test.txt").expect("File not found.");
+    let input_doc = File::open("./src/inputs/Day22input.txt").expect("File not found.");
+    let reader = BufReader::new(input_doc);
+
+    for line in reader.lines(){
+        let line_str = line.expect("Could not parse line");
+        let mut new_brick:Vec<[i16;3]>=Vec::new();
+        let split:Vec<&str> = line_str.split("~").collect();
+        let start_split:Vec<&str> = split[0].split(",").collect();
+        let end_split:Vec<&str> = split[1].split(",").collect();
+        let mut start_point:[i16;3] = [0,0,0];
+        let mut end_point:[i16;3]=[0,0,0];
+        for s in 0..start_split.len(){
+            start_point[s] = start_split[s].parse().expect("failed to parse start i16");
+        }
+        for s in 0..end_split.len(){
+            end_point[s] = end_split[s].parse().expect("failed to parse end i16");
+        }
+        if start_point[0]!=end_point[0]{
+            let start = cmp::min(start_point[0], end_point[0]);
+            let end = cmp::max(start_point[0], end_point[0]);
+            for i in start..=end{
+                new_brick.push([i,start_point[1],start_point[2]]);
+            }
+        }   
+        else if start_point[1]!= end_point[1]{
+            let start = cmp::min(start_point[1], end_point[1]);
+            let end = cmp::max(start_point[1], end_point[1]);
+            for i in start..=end{
+                new_brick.push([start_point[0],i,start_point[2]]);
+            }
+        }
+        else if start_point[2]!= end_point[1]{
+            let start = cmp::min(start_point[2], end_point[2]);
+            let end = cmp::max(start_point[2], end_point[2]);
+            for i in start..=end{
+                new_brick.push([start_point[0],start_point[1],i]);
+            }
+        }
+        else{
+            new_brick.push(start_point);
+        }
+        output.push(new_brick);
+    }
+
+    return output;
 }
