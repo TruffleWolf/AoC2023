610e041a04e92458600e026ce358e67e1523e8de
TruffleWolf <life4pylo@gmail.com>
2025-01-20 14:33:46 +0000

    Day25

--- src/days/day24.rs
+++ src/days/day24.rs
@@ -1,9 +1,91 @@
-
+use std::fs::File;
+use std::io::{BufRead,BufReader};
 
 pub fn part1(){
     println!("Day24 Part1");
+    let (pos_data,vel_data)=parse_document();
+    
+    let mut total:u32 = 0;
+    let test_area = [200000000000000.0,400000000000000.0];
+    for a in 0..pos_data.len(){
+        for b in a+1..pos_data.len(){
+            if a!=b{
+                let denom:f64 = vel_data[b][1]*vel_data[a][0]-vel_data[b][0]*vel_data[a][1];
+                if denom==0.0{continue;}//parallel
+                let coords = [pos_data[a][0]-pos_data[b][0],pos_data[a][1]-pos_data[b][1]];
+                let off:f64 = (vel_data[b][0]*coords[1]-vel_data[b][1]*coords[0])/denom;
+                let intersect = [pos_data[a][0]+off*vel_data[a][0],pos_data[a][1]+off*vel_data[a][1]];
+                //println!("{}/{}-{:?} From off{}",a,b,intersect,off);
+                if intersect[0]>=test_area[0]&&intersect[0]<=test_area[1]&&intersect[1]>=test_area[0]&&intersect[1]<=test_area[1]{
+                    
+
+                    if (intersect[0]-pos_data[a][0])*vel_data[a][0]>0.0&&(intersect[1]-pos_data[a][1])*vel_data[a][1]>0.0{
+                        // in A's future
+                        if (intersect[0]-pos_data[b][0])*vel_data[b][0]>0.0&&(intersect[1]-pos_data[b][1])*vel_data[b][1]>0.0{
+                            //in B's future
+                            total+=1;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    println!("Total:{}",total);
 }
 
 pub fn part2(){
+    
     println!("Day24 Part2");
+
+//     pts = ToExpression["{" <> StringReplace[#, "@" -> ","] <> "}"] & /@ 
+//     Import["D:\\CodeRepos\\Rust\\AoC2023\\src\\inputs\\Day24input.txt",
+//       "Lines"];
+ 
+//  AtTime[pt_, t_] := {pt[[1]] + t*pt[[4]], pt[[2]] + t*pt[[5]], pt[[3]] + t*pt[[6]]}
+//  magicRock = {sx, sy, sz, dx, dy, dz};
+//  part2solution = 
+//    Solve[(AtTime[magicRock, t1] == AtTime[pts[[1]], t1] && 
+//       AtTime[magicRock, t2] == AtTime[pts[[2]], t2] && 
+//       AtTime[magicRock, t3] == AtTime[pts[[3]], t3]), {sx, sy, sz, dx, 
+//      dy, dz, t1, t2, t3}];
+ 
+// Print["Part 2 is ", sx + sy + sz /. part2solution[[1]]];
+
+    println!("Solved with Mathematica, will not work with alt inputs");
+    println!("Total:871983857253169");
+    
+
+}
+
+
+fn parse_document()->(Vec<[f64;3]>,Vec<[f64;3]>){
+
+    //let input_doc = File::open("./src/inputs//test/Day24test.txt").expect("File not found.");
+    let input_doc = File::open("./src/inputs/Day24input.txt").expect("File not found.");
+    let reader = BufReader::new(input_doc);
+    let mut pos:Vec<[f64;3]>=Vec::new();
+    let mut vel:Vec<[f64;3]>=Vec::new();
+    for line in reader.lines(){
+        let line_str = line.expect("Could not parse line");
+        let split1:Vec<&str> = line_str.split(" @ ").collect();
+        let pos_split:Vec<&str>=split1[0].split(", ").collect();
+        let vel_split:Vec<&str>=split1[1].split(", ").collect();
+        let mut new_pos:[f64;3]=[0.0,0.0,0.0];
+        let mut new_vel:[f64;3]=[0.0,0.0,0.0];
+        
+        for p in 0..pos_split.len(){
+            let new_str = pos_split[p].trim();
+            new_pos[p]=new_str.parse::<f64>().expect("Failed to parse position.");
+        }
+        
+        for v in 0..vel_split.len(){
+            let new_str = vel_split[v].trim();
+            new_vel[v]=new_str.parse::<f64>().expect("Failed to parse veloctiy.");
+        }
+        pos.push(new_pos);
+        vel.push(new_vel);
+    }
+
+    return (pos,vel);
 }
--- src/days/day25.rs
+++ src/days/day25.rs
@@ -1,9 +1,94 @@
-
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead,BufReader};
 
 pub fn part1(){
     println!("Day25 Part1");
+    let mut connections = parse_document();
+    let mut list:Vec<String> = Vec::new();
+
+    for c in &connections{
+        if !list.contains(&c[1]){
+            list.push(c[1].clone());
+        }
+        if !list.contains(&c[0]){
+            list.push(c[0].clone())
+        }
+    }
+    println!("Count:{}",list.len());
+
+    let mut group_1:HashSet<String>=HashSet::with_capacity(3);
+    let mut group_2:HashSet<String>=HashSet::with_capacity(3);
+    group_1.insert(String::from("mgb"));
+    group_1.insert(String::from("qns"));
+    group_1.insert(String::from("tjd"));
+    group_2.insert(String::from("plt"));
+    group_2.insert(String::from("jxm"));
+    group_2.insert(String::from("dbt"));
+
+    while connections.len()>0{
+        let mut new_connections:Vec<[String;2]>=Vec::new();
+
+        for c in &connections{
+            if group_1.contains(&c[0]){
+                group_1.insert(c[1].clone());
+            }
+            else if group_1.contains(&c[1]){
+                group_1.insert(c[0].clone());
+            }
+            else if group_2.contains(&c[0]){
+                group_2.insert(c[1].clone());
+            }
+            else if group_2.contains(&c[1]){
+                group_2.insert(c[0].clone());
+            }
+            else{
+                new_connections.push(c.clone());
+            }
+
+        }
+
+
+        connections = new_connections;
+    }
+
+
+    let total = group_1.len()*group_2.len();
+    println!("Total:{} from {}x{}",total,group_1.len(),group_2.len());
 }
 
 pub fn part2(){
     println!("Day25 Part2");
+}
+
+fn parse_document()->Vec<[String;2]>{
+    let mut output:Vec<[String;2]>=Vec::new();
+    //let input_doc = File::open("./src/inputs//test/Day25test.txt").expect("File not found.");
+    let input_doc = File::open("./src/inputs/Day25input.txt").expect("File not found.");
+    let reader = BufReader::new(input_doc);
+
+    for line in reader.lines(){
+        let line_str = line.expect("Could not parse line");
+        let split1:Vec<&str>=line_str.split(": ").collect();
+        let split2:Vec<&str>=split1[1].split(" ").collect();
+        // use this to find the three splits by feeding the result into graphviz
+        
+        // for s in split2{
+        //     println!("{} -> {}",split1[0],s);
+        // }
+        // continue;
+
+        let connect_1 = [String::from("mgb"),String::from("plt")];
+        let connect_2 = [String::from("qns"),String::from("jxm")];
+        let connect_3 = [String::from("tjd"),String::from("dbt")];
+        let connect_4 = [String::from("plt"),String::from("mgb")];
+        let connect_5 = [String::from("jxm"),String::from("qns")];
+        let connect_6 = [String::from("dbt"),String::from("tjd")];
+        let invalid_connections:Vec<[String;2]>=vec![connect_1,connect_2,connect_3,connect_4,connect_5,connect_6];
+        for s in split2{
+            let new_connection =[String::from(split1[0]),String::from(s)];
+            if !invalid_connections.contains(&new_connection){output.push(new_connection);}
+        }
+    }
+    return output;
 }
